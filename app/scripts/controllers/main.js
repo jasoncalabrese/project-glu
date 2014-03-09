ed978c25517bf5adca35e5c6e1ef62dbf5ad27e9
Jason Calabrese <jason.calabrese@refworks-cos.com>
2014-03-09 00:09:06 -0800

    slightly better display of entries, next step is using d3

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,4 +11,21 @@ angular.module('projectGluApp')
 
     update();
 
+    var mapping = {
+      'NONE': '&#8596;',
+      'DoubleUp': '&#8648;',
+      'SingleUp': '&#8593;',
+      'FortyFiveUp': '&#8599;',
+      'Flat': '&#8594;',
+      'FortyFiveDown': '&#8600;',
+      'SingleDown': '&#8595;',
+      'DoubleDown': '&#8650;',
+      'NOT COMPUTABLE': '???',
+      'RATE OUT OF RANGE': '&#8622;'
+    };
+
+    $scope.directionToChar = function(direction) {
+      return mapping[direction];
+    };
+
   });
