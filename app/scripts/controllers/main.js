40ab42bc545da52ac5388ba7d68a362169db0115
Jason Calabrese <jason.calabrese@refworks-cos.com>
2014-03-11 22:15:07 -0700

    only return entries when logged in; some clean up before init push to aws

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,21 @@
 'use strict';
 
 angular.module('projectGluApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $filter, $timeout) {
 
     function update() {
       $http.get('/api/entries').success(function(entries) {
         $scope.entries = entries;
+        $scope.lastDay = '';
       });
     }
 
-    update();
+    function startUpdateCycle() {
+      update();
+      $timeout(startUpdateCycle, 5 * 60 * 100);
+    }
+
+    startUpdateCycle();
 
     var mapping = {
       'NONE': '&#8700;',
@@ -22,6 +28,21 @@ angular.module('projectGluApp')
       'DoubleDown': '&#8650;',
       'NOT COMPUTABLE': '&#10568;',
       'RATE OUT OF RANGE': '&#8622;'
+    };
+
+    var dateFilter = $filter('date');
+
+    $scope.showDay = function(entry) {
+      $scope.currentDay = dateFilter(entry.timestamp);
+
+      if ($scope.currentDay !== $scope.lastDay) {
+        console.info('compared "' + $scope.currentDay + '" to "' + $scope.lastDay);
+        $scope.lastDay = $scope.currentDay;
+        return true;
+      } else {
+        return false;
+      }
+
     };
 
     $scope.directionToChar = function(direction) {
